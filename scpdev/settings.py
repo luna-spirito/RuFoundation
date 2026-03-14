2d65b797ccc403d53b7caa2d07f80d5c8f83aaea
Вован <vovawees@gmail.com>
2026-03-14 19:34:40 +0100

    Update settings.py

--- scpdev/settings.py
+++ scpdev/settings.py
@@ -18,7 +18,7 @@ import mimetypes
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+DEFAULT_USER_ROLE = os.environ.get('DEFAULT_USER_ROLE', 'reader')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
